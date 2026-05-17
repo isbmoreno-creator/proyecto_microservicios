1bcc49fe5012053cf2ce6103b4a49b457c4d217e
heythis <hey.herrera@duocuc.cl>
2026-05-17 00:32:16 -0400

    primer commit de ms-empleados

---
+++ ms-empleados/ms-empleados/src/main/java/com/duoc/msempleados/model/EmpleadosModel.java
@@ -0,0 +1,93 @@
+package com.duoc.msempleados.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "empleado")
+public class EmpleadosModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String cargo;
+    private String aniosExperencia;
+    private Double salario;
+    private Boolean activo;
+    private LocalDate fechaContratacion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getAniosExperencia() {
+        return aniosExperencia;
+    }
+
+    public void setAniosExperencia(String aniosExperencia) {
+        this.aniosExperencia = aniosExperencia;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    public LocalDate getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setFechaContratacion(LocalDate fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+}
---
+++ ms-empleados/ms-empleados/src/main/java/com/duoc/msempleados/repository/EmpleadoRepository.java
@@ -0,0 +1,16 @@
+package com.duoc.msempleados.repository;
+
+import com.duoc.msempleados.model.EmpleadosModel;
+import feign.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+
+public interface EmpleadoRepository extends JpaRepository<EmpleadosModel, Integer > {
+ @Query(value = "SELECT * FROM empleado WHERE activo = true AND YEAR(fecha_contratacion) = :anio", nativeQuery = true)
+ List<EmpleadosModel> findEmpleadoActivosPorAnio(@Param("anio") Integer anio);
+}
