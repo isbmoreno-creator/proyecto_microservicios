fba68a1108815dfab0c4089fd7be2faae6c6f44b
heythis <hey.herrera@duocuc.cl>
2026-06-21 19:11:05 -0400

    Se agrega swagger,HATEOAS, tests y Eureka Client en msreservas

--- msreservas/src/main/java/com/duoc/msreservas/controller/EstadoReservaController.java
+++ msreservas/src/main/java/com/duoc/msreservas/controller/EstadoReservaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/estadoReserva")
+@RequestMapping("/api/v1/estadoReserva")
 public class EstadoReservaController {
 
     @Autowired
